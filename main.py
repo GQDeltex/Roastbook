d9aa1ac6922ead733ccb284b30ca1fa63cd770ec
Peter Stengl <peter@5stengl.de>
2018-12-07 19:12:25 +0100

    Added Rethinkdb and Config File to configure the database

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
  # -*- coding: utf-8 -*-
 from flask import Flask, request, redirect, url_for, render_template, make_response, Markup
-import sqlite3
+import rethinkdb as r
 import json
 import hashlib
 
+config = {
+    'host':'localhost',
+    'user':'admin',
+    'password':''
+}
+
+with open('config.json') as f:
+    config = json.loads(f.read())
+
 app = Flask(__name__)
-conn = sqlite3.connect("./data.sqlite3")
-c = conn.cursor()
-#c.execute("DROP TABLE users")
-#c.execute("CREATE TABLE users (username VARCHAR(20), password VARCHAR(20), balance INT, level INT, liked TEXT, PRIMARY KEY(username))")
-#c.execute("DROP TABLE posts")
-#c.execute("CREATE TABLE posts (id INT, content TEXT, upvote INT, downvote INT, user VARCHAR(20), roasted VARCHAR(20), PRIMARY KEY(id))")
-#c.execute("UPDATE users SET liked=', '")
-#conn.commit()
+connection = r.connect(host=config['host'], user=config['user'], password=config['password'], db='roastbook').repl()
+print(connection.server())
+#r.db_create('roastbook').run()
+connection.use('roastbook')
+#r.table_drop('users').run()
+#r.table_create('users').run()
+#r.table_drop('posts').run()
+#r.table_create('posts').run()
+#r.table('users').update({'liked':[]}).run()
+#r.table('users').index_create('username').run()
 
 @app.route("/")
 def index():
-    top_user_raw = get_data("SELECT username, balance FROM users ORDER BY balance DESC LIMIT 5")
-    top_user = []
-    for item in top_user_raw:
-        top_user.append([(list(item)[0]), str(list(item)[1])])
+    top_user = r.table('users').order_by(r.desc('balance')).limit(5).pluck(['username', 'balance']).run(connection)
     #print("Top Users: " + str(top_user))
-    top_roast_raw = get_data("SELECT username, balance FROM users ORDER BY balance ASC LIMIT 5")
-    top_roast = []
-    for item in top_roast_raw:
-        top_roast.append([(list(item)[0]), str(list(item)[1])])
+    top_roast = r.table('users').order_by(r.asc('balance')).limit(5).pluck(['username', 'balance']).run(connection)
     #print("Top Roasted: " + str(top_roast))
-    top_post_raw = get_data("SELECT * FROM posts ORDER BY id DESC LIMIT 100")
-    top_post = []
-    for item in top_post_raw:
-        item = list(item)
-        if item[3]+item[2] != 0:
-            prc1 = float(item[3]/float(item[3]+item[2]))*100
-            prc2 = float(item[2]/float(item[3]+item[2]))*100
-            if prc1 <= 10:
-                prc1 = 10.0
-                prc2 = 90.0
-            if prc2 <= 10:
-                prc2 = 10.0
-                prc1 = 90.0
-            item.append(prc1)
-            item.append(prc2)
-        else:
-            item.append(50)
-            item.append(50)
-        top_post.append(list(item))
+    top_post = r.table('posts').order_by(r.desc('id')).limit(100).run(connection)
     #print("Top Posts: " + str(top_post))
+    for post in top_post:
+        if (post['upvote']+post['downvote']) != 0:
+            post['up_perc'] = (post['upvote']/(post['upvote']+post['downvote']))*100
+            post['down_perc'] = (post['downvote']/(post['upvote']+post['downvote']))*100
+        else:
+            post['up_perc'] = 50
+            post['down_perc'] = 50
+        if post['up_perc'] < 10:
+            post['up_perc'] = 10
+            post['down_perc'] = 90
+        if post['down_perc'] < 10:
+            post['down_perc'] = 10
+            post['up_perc'] = 90
     return render_template("homepage.html", top_user=top_user, top_roast=top_roast, top_post=top_post)
 
 @app.route("/login", methods=["POST", "GET"])
@@ -54,25 +54,28 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        try:
-            data = list(get_data("SELECT * FROM users WHERE username=?", username)[0])
-        except Exception as e:
-            error = "Dich gibts anscheinen ned oder du bischt zu dumm zum tippen"
-            print e
+        user_data = r.table('users').get_all(username, index='username').coerce_to('array').run(connection)
+        if len(user_data) == 0:
+            error = "Nutzer nicht gefunden!"
+        elif len(user_data) > 1:
+            error = "Mehrere Nutzer mit gleichem Namen gefunden, bitte kontaktieren sie den support oder nutzen sie ihre ID als nutzername (z.B. 'id-c73a8f94-a5d6-4234-b88a-a8e5b65ebd88')."
         else:
+            data = user_data[0]
             pwd_hash = hashlib.sha256(username + password).hexdigest()
-            #print ("ORIG:\t" + str(data[1]))
+            #print ("ORIG:\t" + str(data['password']))
             #print ("NEW:\t" + str(pwd_hash))
-            if data[1] == pwd_hash:
+            if data['password'] == pwd_hash:
                 resp = make_response(redirect(url_for("user", username=username)))
                 resp.set_cookie("user", value=username)
+                resp.set_cookie("user_id", value=data['id'])
                 return resp
             else:
-                error = "Dein Passwort ischt verdraeht"
+                error = "Falsches Password."
     else:
         if request.cookies.get("user"):
             resp = make_response(redirect(url_for("index")))
             resp.set_cookie("user", value="")
+            resp.set_cookie("user_id", value="")
             return resp
     return render_template("login.html", error=error)
 
@@ -80,135 +83,126 @@ def login():
 def register():
     error = ""
     if request.method == "POST":
-        username = Markup.escape(request.form["username"])
+        username = str(Markup.escape(request.form["username"]))
         password = request.form["password"]
         password_repeat = request.form["password_repeat"]
-        data = getuser(username)
-        if data != None:
-            print (data)
-            error = "!!!!!!You shall not pass!!!!!!!"
+        if r.table('users').filter({'username':username}).count().run(connection) != 0:
+            error = "Ein Benutzer mit deinem Benutzernamen existiert bereits, suche dir einen anderen aus."
         elif password != password_repeat:
-            error = "Dein Passwort isch verdraeht!"
+            error = "Die beiden eingegebenen Passwoerter stimmen nicht ueberein!"
         else:
             pwd_hash = hashlib.sha256(username + password).hexdigest()
-            c.execute("INSERT INTO users (username, password, balance, level) VALUES (?, ?, ?, ?)", (username, pwd_hash, 0, 0))
-            conn.commit()
-            print ("New User: " + username)
+            user_id = r.table('users').insert({'username':username, 'password':pwd_hash, 'balance':0, 'liked':[]}).run(connection)['generated_keys'][0]
+            print ("New User: %s, id: %s" % (username, user_id))
             resp = make_response(redirect(url_for("user", username=username)))
             resp.set_cookie("user", value=username)
+            resp.set_cookie("user_id", value=user_id)
             return resp
     return render_template("register.html", error=error)
 
 @app.route("/users/<username>")
 def user(username):
-    data = getuser(username)
-    if data != None:
-        posts_raw = get_data("SELECT * FROM posts WHERE user=? OR roasted=? ORDER BY id DESC", (username, username))
-        posts = []
-        for item in posts_raw:
-            item = list(item)
-            if item[3]+item[2] != 0:
-                prc1 = float(item[3]/float(item[3]+item[2]))*100
-                prc2 = float(item[2]/float(item[3]+item[2]))*100
-                if prc1 <= 10:
-                    prc1 = 10.0
-                    prc2 = 90.0
-                if prc2 <= 10:
-                    prc2 = 10.0
-                    prc1 = 90.0
-                item.append(prc1)
-                item.append(prc2)
+    data = r.table('users').get_all(username, index='username').coerce_to('array').run(connection)
+    if len(data) == 1:
+        posts = r.table('posts').filter((r.row['from'] == username) | (r.row['to'] == username)).order_by(r.desc('id')).run(connection)
+        for post in posts:
+            if (post['upvote']+post['downvote']) != 0:
+                post['up_perc'] = (post['upvote']/(post['upvote']+post['downvote']))*100
+                post['down_perc'] = (post['downvote']/(post['upvote']+post['downvote']))*100
             else:
-                item.append(50)
-                item.append(50)
-            posts.append(list(item))
-        all_names = json.dumps(get_data("SELECT username FROM users"))
-        return render_template("user.html", user=data, you=True, posts=posts, all_names=all_names)
+                post['up_perc'] = 50
+                post['down_perc'] = 50
+            if post['up_perc'] < 10:
+                post['up_perc'] = 10
+                post['down_perc'] = 90
+            if post['down_perc'] < 10:
+                post['down_perc'] = 10
+                post['up_perc'] = 90
+        all_names = json.dumps(r.table('users').pluck('username', 'id').coerce_to('array').run(connection))
+        return render_template("user.html", user=data[0], posts=posts, all_names=all_names)
     else:
         return "Diesen Benutzer gibt es nicht!"
 
 @app.route("/edit_post/<id>", methods=["GET", "POST"])
 def edit_post(id):
-    data = get_data("SELECT * FROM posts WHERE id=?", id)[0]
-    if request.cookies.get("user") != data[4]:
+    data = r.table('posts').get(id).run(connection)
+    if request.cookies.get("user_id") != data['from_id']:
         return "You are not allowed to do this!\r\n<a href='" + url_for('index') + "'>Back to Homepage</a>"
     if request.method == "POST":
-        text = Markup.escape(request.form['text'])
-        c.execute("UPDATE posts SET content=? WHERE id=?", (text, id))
-        conn.commit()
+        text = str(Markup.escape(request.form['text']))
+        r.table('posts').get(id).update({'text':text}).run(connection)
         return redirect(url_for('user', username=request.cookies.get("user")))
     else:
         return render_template("edit.html", data=data)
 
-
 @app.route("/upvote")
 def upvote():
     user = request.cookies.get("user")
+    user_id = request.cookies.get("user_id")
     if user == "":
         return "You are not allowed to do this!\r\n<a href='" + url_for('index') + "'>Back to Homepage</a>"
-    id = int(request.args.get("id"))
+    id = request.args.get("id")
     source = request.args.get("source", default=url_for("index"))
-    username, upvote, = get_data("SELECT user, upvote FROM posts WHERE id=?", int(id))[0]
-    balance, = get_data("SELECT balance FROM users WHERE username=?", str(username))[0]
-    liked, = get_data("SELECT liked FROM users WHERE username=?", user)[0]
-    if liked != None:
-        if liked.find(", " + str(id) + ", ") != -1:
+    data = r.table('posts').get(id).pluck('from_id', 'upvote').run(connection)
+    username, upvote = data['from_id'], data['upvote']
+    balance = r.table('users').get(username).pluck('balance').run(connection)['balance']
+    liked = r.table('users').get(user_id).pluck('liked').run(connection)['liked']
+    if liked != []:
+        if id in liked:
             return redirect(source)
     else:
-        liked = ", "
-    liked += (str(id) + ", ")
+        liked = []
+    liked.append(id)
     #print (str(liked))
-    print (user + " liked post nr. " + str(id))
+    print ("%s liked post %s" % (user, id))
     balance += 1
     upvote += 1
-    print ("Balance of %s changed from %d to %d" % (username, balance-1, balance))
-    c.execute("UPDATE users SET balance=? WHERE username=?", (balance, username))
-    c.execute("UPDATE users SET liked=? WHERE username=?", (liked, user))
-    c.execute("UPDATE posts SET upvote=? WHERE id=?", (upvote, id))
-    conn.commit()
+    print ("Balance of %s changed from %d to %d" % (user_id, balance-1, balance))
+    r.table('users').get(user_id).update({'balance':balance}).run(connection)
+    r.table('users').get_all(user, index='username').update({'liked':liked}).run(connection)
+    r.table('posts').get(id).update({'upvote':upvote}).run(connection)
     return redirect(source)
 
 @app.route("/downvote")
 def downvote():
     user = request.cookies.get("user")
+    user_id = request.cookies.get("user_id")
     if user == "":
         return "You are not allowed to do this!\r\n<a href='" + url_for('index') + "'>Back to Homepage</a>"
-    id = int(request.args.get("id"))
+    id = request.args.get("id")
     source = request.args.get("source", default=url_for("index"))
-    username, downvote = get_data("SELECT user,downvote FROM posts WHERE id=?", int(id))[0]
-    balance, = get_data("SELECT balance FROM users WHERE username=?", str(username))[0]
-    liked, = get_data("SELECT liked FROM users WHERE username=?", user)[0]
-    if liked != None:
-        if liked.find(", " + str(id) + ", ") != -1:
+    data = r.table('posts').get(id).pluck('from_id', 'downvote').run(connection)
+    username, downvote = data['from_id'], data['downvote']
+    balance = r.table('users').get(username).pluck('balance').run(connection)['balance']
+    liked = r.table('users').get(user_id).pluck('liked').run(connection)['liked']
+    if liked != []:
+        if id in liked:
             return redirect(source)
     else:
-        liked = ", "
-    liked += (str(id) + ", ")
-    #print str(liked)
-    print (user + " disliked post nr. " + str(id))
+        liked = []
+    liked.append(id)
+    #print (str(liked))
+    print ("%s disliked post %s" % (user, id))
     balance -= 1
     downvote += 1
-    print ("Balance of %s changed from %d to %d" % (username, balance+1, balance))
-    c.execute("UPDATE users SET balance=? WHERE username=?", (balance, username))
-    c.execute("UPDATE users SET liked=? WHERE username=?", (liked, user))
-    c.execute("UPDATE posts SET downvote=? WHERE id=?", (downvote, id))
-    conn.commit()
+    print ("Balance of %s changed from %d to %d" % (user_id, balance+1, balance))
+    r.table('users').get(user_id).update({'balance':balance}).run(connection)
+    r.table('users').get_all(user, index='username').update({'liked':liked}).run(connection)
+    r.table('posts').get(id).update({'downvote':downvote}).run(connection)
     return redirect(source)
 
 @app.route("/newpost", methods=["POST"])
 def newpost():
-    text = Markup.escape(request.form['text'])
-    username = request.form['username']
+    text = str(Markup.escape(request.form['text']))
+    user_id = request.form['username']
+    username = r.table('users').get(user_id).pluck('username').run(connection)['username']
     print("Created Post:" + str(username))
     my_username = request.cookies.get("user")
-    if get_data("SELECT username FROM users WHERE username=?", username):
-        try:
-            id, = get_data("SELECT id FROM posts ORDER BY id DESC LIMIT 1")[0]
-        except:
-            id = 0
-        c.execute("INSERT INTO posts (id, content, upvote, downvote, user, roasted) VALUES (?, ?, 0, 0, ?, ?)", (id+1, text, my_username, username))
-        conn.commit()
+    my_user_id = request.cookies.get("user_id")
+    if r.table('users').get(user_id).pluck('id').count().run(connection) == 1:
+        r.table('posts').insert({'text':text, 'upvote':0, 'downvote':0, 'from':my_username, 'to':username, 'from_id':my_user_id, 'to_id':user_id}).run(connection)
         return redirect(url_for('user', username=my_username))
+    return user_id
 
 @app.route("/agb")
 def agb():
@@ -222,30 +216,5 @@ def impressum():
 def datenschutz():
     return render_template("datenschutz.html")
 
-def get_data(query, args=""):
-    if type(args) != tuple:
-        #print ("Args is not tuple")
-        #print ("Type: " + str(type(args)))
-        #print (str(args))
-        if args != "":
-            adding = (str(args), )
-        else:
-            adding = ""
-    else:
-        adding = args
-    #print (str(adding))
-    data = c.execute(query, adding)
-    return data.fetchall()
-
-def getuser(username):
-    #print (str(username))
-    query = c.execute("SELECT * FROM users WHERE username=?", (username, ))
-    data = query.fetchall()
-    #print data
-    if data == []:
-        return None
-    else:
-        return list(data[0])
-
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=80)
+    app.run("0.0.0.0", port=8000)
